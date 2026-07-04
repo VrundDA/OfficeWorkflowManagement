98af07ff360d9892b27d0250cf7c0a65b2f499d7
VrundDA <202301015@daiict.ac.in>
2026-07-04 10:33:38 +0530

    modified settings file

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -105,8 +109,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "vrundkansaramirror@gmail.com"
-EMAIL_HOST_PASSWORD = "bayeayhsqemprvyf"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_NAME")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # baye ayhs qemp rvyf
